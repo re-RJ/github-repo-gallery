127a36d5004b09a1782e4a8869da9c118ad04844
rj_m <rjmarte@sbcglobal.net>
2026-03-14 08:56:41 -0700

    Repos added; to be pared down

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 //profile info
 const overview = document.querySelector(".overview");
 const username = "re-RJ";
+const reposList = document.querySelector(".repo-list");
 
 const gitFetch = async function () {
-    const portfolioArray = await fetch(`https://api.github.com/users/${username}`);
-    const data = await portfolioArray.json();
+    const gitProfile = await fetch(`https://api.github.com/users/${username}`);
+    const data = await gitProfile.json();
     //console.log(data)
     displayInfo(data);
 };
@@ -16,7 +17,7 @@ const displayInfo = function (data) {
     const gitLocation = data.location;
     const gitPubRepos = data.public_repos;
     const avatar = data.avatar_url;
-    
+
     const userInfo = document.createElement("div")
     userInfo.classList.add("user-info");
     userInfo.innerHTML = `
@@ -31,4 +32,24 @@ const displayInfo = function (data) {
         </div> 
     `;
     overview.append(userInfo);
-}
+    gitRepos();
+};
+
+const gitRepos = async function () {
+    const repoSort = await fetch(`https://api.github.com/users/${username}/repos?sort=updated_at&per_page=100`);
+    const repoArray = await repoSort.json();
+    //console.log(repoArray);    
+    displayRepos(repoArray);
+};
+//gitRepos();
+
+const displayRepos = function (repos) {
+    for (const unit of repos) {
+        const li = document.createElement("li");
+        li.classList.add ("unit");
+        li.innerHTML = `
+            <h3>${unit.name}</h3>
+        `
+        reposList.append(li);
+    }
+};
